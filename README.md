bd52bfc7affac4d8b2323f749f45cf842626b4b5
dtwozero <dtwozero@protonmail.com>
2018-12-12 15:52:12 -0500

    added some checks so script handles errors better

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 # amimullvad.py
-This is a python script that verifies from your terminal if you're in fact connected to mullvad's VPN
+![Alt text] (/img/ami_screenshot.png?raw=true "AmiMullvad")
 
+Some of us are already in our terminal and need confirmation that we're connected to our vpn. Opening a browser and going ot the am.i.mullvad.net link isn't always fast enough. This python script verifies from your terminal if you're in fact connected to mullvad's VPN.
 All you have to do is connect to mullvad however you have your machine configured to do and run this script to verify that it's working correctly.
 
-This script requires json, requests, os, and termcolor to run. I think most of you should already have most of these already installed. if not just run pip install termcolor. 
+This script requires `json, requests, os,` and `termcolor` to run. I think most of you should already have most of these already installed. 
 
-I know this will work for most distros without issue, especially since i wrote this on and for my Arch Linux machine.
+`pip install termcolor`
+`pip install json`
+`pip install requests`
+
+### Usage:
+
+`./amimullvad.py`
+
+That's it. Enjoy!
--- amimullvad.py
+++ amimullvad.py
@@ -11,37 +11,46 @@ response = requests.get("https://am.i.mullvad.net/json")
 data = response.json()
 
 # Define a bunch of variables because termcolor doesn't work with json objects
-
-newip = data["ip"]
-country = data["country"]
-city = str(data["city"])
-mullserv = data["mullvad_server_type"]
-latitude = str(data["latitude"])
-longitude = str(data["longitude"])
-mullxip = str(data["mullvad_exit_ip"]) # this is going to be True/False
-mullxhost = data["mullvad_exit_ip_hostname"]
-organization = data["organization"]
+try:
+    newip = data["ip"]
+    country = data["country"]
+    city = str(data["city"])
+    mullserv = data["mullvad_server_type"]
+    latitude = str(data["latitude"])
+    longitude = str(data["longitude"])
+    mullxip = str(data["mullvad_exit_ip"]) # this is going to be True/False
+    mullxhost = data["mullvad_exit_ip_hostname"]
+    organization = data["organization"]
+except:
+    print("nope")
 
 # Print the info we need to the screen
 
 os.system('clear')
-os.system('figlet am.i.mullvad')
+try:
+    os.system('figlet am.i.mullvad')
+except:
+    print(colored("You must install figlet from your Linux Distros Repo.", 'red'))
+
 print("Author: " + colored("Twitter: @dTwoZ3r0", 'red'))
 print("")
-print("IP: " + colored(newip, 'green'))
-print("Country: " + colored(country, 'green'))
-if city == 'None':
-    print("City: " + colored(city, 'red'))
-else:
-    print("City: " + colored(city, 'green'))
-print("Latitude: " + colored(latitude, 'yellow'))
-print("Longitude: " + colored(longitude, 'yellow'))
-if mullxip == False:
-    print("Mullvad Exit IP: " + colored(mullxip, 'red'))
-else:
-    print("Mullvad Exit IP: " + colored(mullxip, 'green'))
-print("Mullvad Exit IP Hostname: " + colored(mullxhost, 'green'))
-print("Organization: " + colored(organization, 'green'))
-print("Mullvad Server Type: " + colored(mullserv, 'green'))
-print("")
-print("To proxy firefox: " + colored("https://mullvad.net/en/servers/#wireguard ", 'green') + "and check Proxy DNS box")
+try:
+    print("IP: " + colored(newip, 'green'))
+    print("Country: " + colored(country, 'green'))
+    if city == 'None':
+        print("City: " + colored(city, 'red'))
+    else:
+        print("City: " + colored(city, 'green'))
+    print("Latitude: " + colored(latitude, 'yellow'))
+    print("Longitude: " + colored(longitude, 'yellow'))
+    if mullxip == False:
+        print("Mullvad Exit IP: " + colored(mullxip, 'red'))
+    else:
+        print("Mullvad Exit IP: " + colored(mullxip, 'green'))
+    print("Mullvad Exit IP Hostname: " + colored(mullxhost, 'green'))
+    print("Organization: " + colored(organization, 'green'))
+    print("Mullvad Server Type: " + colored(mullserv, 'green'))
+    print("")
+    print("To proxy firefox: " + colored("https://mullvad.net/en/servers/#wireguard ", 'green') + "and check Proxy DNS box")
+except:
+    print(colored("You ARE NOT connected to mullvad!!", 'red'))
